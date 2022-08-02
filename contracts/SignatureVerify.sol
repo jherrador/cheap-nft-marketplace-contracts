56ae855b2465699fb0715e5b996e7d9a0711d6c2
Javier Herrador Cuartero <qherra@gmail.com>
2022-08-02 20:31:06 +0200

    WIP Signature

--- contracts/SignatureVerify.sol
+++ contracts/SignatureVerify.sol
@@ -2,46 +2,50 @@
 pragma solidity ^0.8.0;
 
 contract SignatureVerify {
-    function VerifyMessage(
-        string memory _hashedMessage,
-        bytes memory _signature
-    ) public pure returns (address) {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-        (v, r, s) = splitSignature(_signature);
-        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes32 prefixedHashMessage = keccak256(
-            abi.encodePacked(prefix, _hashedMessage)
-        );
-        address signer = ecrecover(prefixedHashMessage, v, r, s);
-        return signer;
+
+    struct Auction {
+        uint256 tokenId;
+        address contractAddress;
+        address ownerAddress;
+        address bidderAddress;
+        uint256 bid;
+    }
+    string private constant types;
+    bytes32 private constant DOMAIN_SEPARATOR;
+    string private constant EIP712_DOMAIN = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+
+
+    constructor(address verifyingContract, string memory appName, string memory version, uint256 chainId, string memory data_type) {
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            EIP712_DOMAIN,
+            keccak256(appName),
+            keccak256(version),
+            chainId,
+            verifyingContract
+        ));
     }
 
-    function splitSignature(bytes memory signatureHash)
-        internal
-        pure
-        returns (
-            uint8,
-            bytes32,
-            bytes32
-        )
-    {
-        require(signatureHash.length == 65);
 
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
+    function hashAuction(Auction auction) private pure returns (bytes32) {
+        return keccak256(abi.encode(
+            AUCTION_TYPE,
+            auction.tokenId,
+            auction.contractAddress,
+            auction.ownerAddress,
+            auction.bidderAddress,
+            auction.bid
+        ));
+    }
 
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(signatureHash, 32))
-            // second 32 bytes
-            s := mload(add(signatureHash, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(signatureHash, 96)))
-        }
+    function hashMessage(Auction memory auction) private pure returns (bytes32){
+        return keccak256(abi.encodePacked(
+            "\\x19\\x01",
+            DOMAIN_SEPARATOR,
+            hashAuction(auction)
+        ));
+    }
 
-        return (v, r, s);
+    function verify(Auction memory auction, address signer, sigR, sigS, sigV) public pure returns (bool) {
+        return signer == ecrecover(hashMessage(auction), sigV, sigR, sigS);
     }
 }
